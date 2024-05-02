9d596e8340639e6152b2ce85cfa7c7cfafec8d42
Scott Jackson <scottrj@umd.edu>
2024-05-02 15:15:19 -0400

    added examples for dataframes, reading from file, and some basic plots

--- data_analysis_examples.py
+++ data_analysis_examples.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 number_list = [1, 4, 22, 89.33]
 string_list = ["a", "c", "something", "else"]
@@ -53,3 +54,52 @@ print(taxed_prices3)
 taxed_prices3 = taxed_prices3.round(2)
 print(taxed_prices3)
 
+# arange
+range(10)
+print(np.arange(10))
+print(np.arange(3, 9))
+print(np.arange(3, 28, 3))
+
+#############################
+fruit_dictionary = {"apples" : 3.49,
+                "bananas" : 1.79,
+                "strawberries" : 5.99}
+
+fruit_dictionary["bananas"] = 1.59
+
+fruit_names = list(fruit_dictionary.keys())
+
+fruit_data = {"fruit" : fruit_names,
+              "price" : fruit_prices}
+print(fruit_data)
+
+fruit_dataframe = pd.DataFrame(data = fruit_data)
+print(fruit_dataframe)
+
+# using dataframe
+fruit_dataframe.describe()
+# a column is an object called a "Series"
+type(fruit_dataframe["price"])
+fruit_dataframe["price"].dtype
+fruit_dataframe["price"].mean()
+
+# reading a file
+iris_dataframe = pd.read_csv("iris_data.csv")
+print(iris_dataframe)
+iris_dataframe.head(10)
+iris_dataframe.tail(7)
+
+iris_dataframe.describe()
+iris_dataframe["Length"].mean()
+
+# visualizing data with matplotlib
+print(fruit_dataframe)
+
+plt.bar(x = "fruit", height = "price", data = fruit_dataframe)
+plt.show()
+
+plt.scatter(x = "Length", y = "Width", data = iris_dataframe)
+plt.show()
+
+plt.hist("Length", data = iris_dataframe)
+plt.show()
